04a6fcf1802e8758b0479861b83fd29cfc7773aa
ruth-tennyson <ruth.tennyson.20@gmail.com>
2024-04-14 20:27:33 +0530

    14/04

--- 3.c
+++ 3.c
@@ -2,11 +2,14 @@
 int main()
 {
     int i,n,sum=0;
+    float avg;
     printf("\nEnter the no.of elements:");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         sum=sum+i;
     }
+    avg=((float)sum/2);
     printf("\nThe sum of %d nos is:%d",n,sum);
+    printf("\nAverage of %d nos is:%f",n,avg);
 }
