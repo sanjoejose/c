a3ebc82f51937e875dfbf61232899366f80633c6
ruth-tennyson <ruth.tennyson.20@gmail.com>
2024-04-14 19:14:41 +0530

    14/10

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0;
+    printf("\nEnter the first no:");
+    scanf("%d",&a);
+    printf("\nEnter the first no:");
+    scanf("%d",&b);
+    c=(a+b)/2;
+    printf("The average of %d and %d is %d",a,b,c);
+}
