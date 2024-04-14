9f8b66958bbf90f679bb87bd7ad11d85d90c103f
ruth-tennyson <ruth.tennyson.20@gmail.com>
2024-04-14 19:15:58 +0530

    14/10

--- 2.c
+++ 2.c
@@ -4,7 +4,7 @@ int main()
     int a,b,c=0;
     printf("\nEnter the first no:");
     scanf("%d",&a);
-    printf("\nEnter the first no:");
+    printf("\nEnter the second no:");
     scanf("%d",&b);
     c=(a+b)/2;
     printf("The average of %d and %d is %d",a,b,c);
