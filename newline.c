bc525d8c8fd8ae4683b5cc5b1cb77df0d821f822
ruth-tennyson <ruth.tennyson.20@gmail.com>
2024-07-08 02:34:29 +0530

    07/07

---
+++ newline.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch[100],s[100],sen[100];
+    scanf("%[^\n]%*c", ch);
+    scanf("%[^\n]%*c", s);
+    scanf("%[^\n]%*c", sen);
+     printf("%s",ch);
+    printf("\n");
+    printf("%s",s);
+    printf("\n");
+    printf("%s",sen);    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
+// to print sentences in different lines
